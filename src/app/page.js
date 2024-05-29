ed5491d4f627810934cda12371788d46af16c286
Md. Rasel Rana <rasel.learn22@gmail.com>
2024-05-30 02:18:19 +0600

    Loading suspension and the blur data loading is implemented

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import EventList from "@/components/landing/EventList";
 import Header from "@/components/landing/Header";
+import Loading from "@/components/landing/Loading";
+import { Suspense } from "react";
 
 const Home = ({ searchParams: { query } }) => {
   return (
     <section className="container">
       <Header />
-      <EventList query={query} />
+      <Suspense key={query} fallback={<Loading />}>
+        <EventList query={query} />
+      </Suspense>
     </section>
   );
 };
---
+++ src/components/landing/Loading.jsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <div>Event data loading...</div>;
+};
+
+export default Loading;
